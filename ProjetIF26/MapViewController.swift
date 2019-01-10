50b0ce882966e7baade6c58aef435be64dbe2624
Vincent <vincent@vincents-imac.local>
2019-01-10 19:40:03 +0100

    Ajout demande des permissions de localisation

--- ProjetIF26/MapViewController.swift
+++ ProjetIF26/MapViewController.swift
@@ -12,29 +12,32 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let locationUTT = CLLocation(latitude: 48.269126, longitude: 4.066722)
-        
         let regionRadius: CLLocationDistance = 1000
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(locationUTT.coordinate, regionRadius, regionRadius)
-        
         mapView.setRegion(coordinateRegion, animated: false)
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
